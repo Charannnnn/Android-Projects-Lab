2592cca83e142822feebd921a978a701ddcc3a45
koppula Sai Charan <koppulasaicharan001@gmail.com>
2021-10-25 14:33:19 +0530

    hello

--- 25-Oct/4-CRUDOnCSV_SQLite/app/src/main/java/com/example/a4_crudoncsv_sqlite/MainActivity.java
+++ 25-Oct/4-CRUDOnCSV_SQLite/app/src/main/java/com/example/a4_crudoncsv_sqlite/MainActivity.java
@@ -7,14 +7,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    DBHelper dbhelper = new DBHelper(getApplicationContext());
+    DBHelper dbhelper;
     SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dbhelper = new DBHelper(getApplicationContext());
         db = dbhelper.getWritableDatabase();
-
+        
     }
 }
